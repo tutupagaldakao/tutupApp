cb01db43513e0036358bd4bb1db1f682d549b315
Tu Nombre <tu@correo.com>
2025-09-29 13:29:12 +0200

    fix(apphosting): ensure server & config

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.get('/', (_req, res) => res.send('tutupapp-backend OK'));
-app.listen(PORT, () => console.log('Listening on', PORT));
+app.get('/', (_req, res) => res.send('tutupapp-backend: OK'));
+app.get('/healthz', (_req, res) => res.send('ok'));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log('listening on', port));
